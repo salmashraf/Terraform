6cd3e171bcf59d2592b024619f344a0c4a5c2e1a
salmashraf <119735396+salmashraf@users.noreply.github.com>
2023-01-19 11:21:14 +0200

    Update ec2_userdata.tf
    adding a line to allow the public id to appear on the screen.

--- ec2_userdata.tf
+++ ec2_userdata.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "web" {
   ami           = "ami-06878d265978313ca"
   instance_type = "t2.micro"
   subnet_id       = aws_subnet.private_subnet.id
+  associate_public_ip_address=true
+
    
    user_data = <<-EOF
   #!/bin/bash
